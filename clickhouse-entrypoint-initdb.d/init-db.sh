9b3c81c843cb1f96d0aac406fbfb6ab452170646
patraden <denis.patrakhin@gmail.com>
2022-09-18 19:19:21 +0300

    clickhouse docker-compose is up

--- clickhouse-entrypoint-initdb.d/init-db.sh
+++ clickhouse-entrypoint-initdb.d/init-db.sh
@@ -2,6 +2,15 @@
 set -e
 
 clickhouse client -n <<-EOSQL
-	CREATE DATABASE docker;
-	CREATE TABLE docker.docker (x Int32) ENGINE = Log;
-EOSQL
+	create database dwh;
+	create table if not exists dwh.exchange_rates (
+        base FixedString(3) NOT NULL, 
+        code FixedString(3) NOT NULL, 
+        date Date NOT NULL,
+        rate float NULL,
+        __dag_id__ String NOT NULL,
+        __dag_run_id__ String NOT NULL,
+        __dag_run_start_date__ datetime NOT NULL
+        )
+        engine = Log;
+		EOSQL
